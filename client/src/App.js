ffe1079cc9384dece3c46126b1a86b26b28a7c4e
Ranjithkarthi <81366510+Ranjithkarthi@users.noreply.github.com>
2024-08-25 16:46:06 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-// Import the necessary components from the react-router-dom package and other custom components
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -9,7 +8,6 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import "./style.scss";
 
-// Create a Layout component that defines the structure of the web page
 const Layout = () => {
   return (
     <>
@@ -20,12 +18,10 @@ const Layout = () => {
   );
 };
 
-// Define the application routes and components using the createBrowserRouter function
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
-    // Remember that Home, Single post and Write are the pages that will have a Navbar and a Footer
     children: [
       {
         path: "/",
@@ -51,7 +47,6 @@ const router = createBrowserRouter([
   },
 ]);
 
-// Define the App function that returns the RouterProvider component that provides the routing context to the entire app
 function App() {
   return (
     <div className="app">
@@ -62,5 +57,4 @@ function App() {
   );
 }
 
-// Export the App component as the default export
 export default App;
