38e178016c2f609a01a22b2340706a293e4001a4
Ranjithkarthi <81366510+Ranjithkarthi@users.noreply.github.com>
2024-08-25 16:47:01 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,9 +2,7 @@ import React, { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/authContext";
 
-// Define a functional component called Login
 const Login = () => {
-  // Use useState hook to create state variables for inputs and errors
   const [inputs, setInputs] = useState({
     username: "",
     password: "",
@@ -12,31 +10,23 @@ const Login = () => {
 
   const [err, setError] = useState(null);
 
-  // Use useNavigate hook to create a navigate function
   const navigate = useNavigate();
-
-  //useContext hook to get the login function from the AuthContext.
   const { login } = useContext(AuthContext);
 
-  // Define handleChange function to update the input state variables when the user types into the input fields
   const handleChange = (e) => {
     setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   };
 
-  // Define handleSubmit function to handle the form submission when the user clicks the submit button
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      // Post the user input to the "/auth/login" endpoint and navigate to the home page
       await login(inputs);
       navigate("/");
     } catch (err) {
-      // If there is an error, set the error state variable to the error message
       setError(err.response.data);
     }
   };
 
-  // Render the login form with input fields for username and password and a button to submit the form
   return (
     <div className="auth">
       <h1>Login</h1>
